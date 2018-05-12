ebb8226094582ff2fb20633c6c31977345340a19
midstreamer <chadalessi@yahoo.com>
2018-05-12 12:01:37 -0700

    added html slider template files for landing page

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -2,6 +2,12 @@ var path = require("path");
 
 module.exports = function(app) {
 //this will be for creating user profile 
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/assets/home.html"));
+  });
+
+
+
     app.get("/create", function(req, res) {
         res.sendFile(path.join(__dirname, "../public/assets/userProfileInfo.html"));
       });
