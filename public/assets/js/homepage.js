7c5567933beccbb794abf429897ae6a360a2c8c7
CuHTeTuk <cuhtetuk@CuHTeTuks-MacBook-Pro.local>
2018-05-13 13:10:59 -0500

    added loop for pulling sotry to mane page will need some work later

--- public/assets/js/homepage.js
+++ public/assets/js/homepage.js
@@ -7,10 +7,19 @@ $(document).ready(function () {
     function listOfUsers() {
       $.get("/api/blog", function (data) {
         console.log(data)
-        // 
+        // need to fix the size of pic and make it nice will also add blog story
         for (var i = 0; i < data.length; i++) {
-          console.log(data[i])
+          console.log(data[i]) 
+         var imgLocation = $("<div class='profileInfo'>");
+          var picture = $("<img>").addClass("circle").attr("src", data[i].user_pic_profile)
+          var dropTitle = $("<div class = 'raiting drop'>").text("Name"+data[i].user_First_name + "  " + data[i].user_Last_name )
+          imgLocation.append(picture);
+          imgLocation.append(dropTitle);
+          //imgLocation.append(dropRaiting);
+          $("#users_list").append(imgLocation);
          }
+   
+
 
       });
     }
--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var bodyParser = require("body-parser");
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = process.env.PORT || 3309;
+var PORT = process.env.PORT || 3310;
 //var exphbs = require("express-handlebars");
 
 var db = require("./models");
