21060f3624e7849dfb221cc81919cb1aa1f767d7
CuHTeTuk <cuhtetuk@CuHTeTuks-MacBook-Pro.local>
2018-05-13 19:59:11 -0500

    new logic for pulling blogs

--- public/assets/js/homepage.js
+++ public/assets/js/homepage.js
@@ -1,27 +1,26 @@
 $(document).ready(function () {
     console.log("check user home page")
-  
+
     listOfUsers()
-  
+
     // get participant from API
     function listOfUsers() {
-      $.get("/api/blog", function (data) {
-        console.log(data)
-        // need to fix the size of pic and make it nice will also add blog story
-        for (var i = 0; i < data.length; i++) {
-          console.log(data[i]) 
-         var imgLocation = $("<div class='profileInfo'>");
-          var picture = $("<img>").addClass("circle").attr("src", data[i].user_pic_profile)
-          var dropTitle = $("<div class = 'raiting drop'>").text("Name"+data[i].user_First_name + "  " + data[i].user_Last_name )
-          imgLocation.append(picture);
-          imgLocation.append(dropTitle);
-          //imgLocation.append(dropRaiting);
-          $("#users_list").append(imgLocation);
-         }
-   
+        $.get("/api/blog", function (data) {
 
+            for(let i = 0; i < data.length; i++){
 
-      });
+                let childArray = data[i].Blogs;
+                //console log for main data 
+                console.log(data[i])
+                for(let j = 0; j < childArray.length; j++){
+             //pulling blogs 
+               console.log(childArray[j]);
+            
+                }
+             
+             }
+             
+        });
     }
 
-  })
+})
