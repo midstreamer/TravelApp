236971404d9dfe1bc774e9795b5c60facdfc786d
CuHTeTuk <cuhtetuk@CuHTeTuks-MacBook-Pro.local>
2018-05-16 18:09:01 -0500

    added modal for creating new story

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var passport = require("./config/passport");
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = process.env.PORT || 3323;
+var PORT = process.env.PORT || 3324;
 //var exphbs = require("express-handlebars");
 
 var db = require("./models");
