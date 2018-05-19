877f67377973cd9cc2e5d0aa4d608f64089213b9
CuHTeTuk <cuhtetuk@CuHTeTuks-MacBook-Pro.local>
2018-05-19 11:47:14 -0500

    fixed the issue with search

--- public/assets/js/homePageSearch.js
+++ public/assets/js/homePageSearch.js
@@ -6,13 +6,15 @@
 
     function participantSearchHome() {
 
-        console.log("storySubmit")
         event.preventDefault();
         var search_input = $("#search_input").val().trim();
         console.log(search_input)
 
-
-        $.get("/api/homePage/search", search_input, function (data, cb) {
+            var newSearch_input ={
+                newSearch_input:search_input
+            }
+console.log(search_input)
+        $.get("/api/homepage/search", newSearch_input, function (data, cb) {
 console.log(data)
         }
         ).then(function (data) {
