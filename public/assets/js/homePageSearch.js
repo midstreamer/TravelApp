037381d7b24f02634d3eaa05328e606be462041f
Rohan Menon <rohan@Rohans-MacBook-Pro.local>
2018-05-19 14:04:28 -0500

    globe functionality done

--- public/assets/js/homePageSearch.js
+++ public/assets/js/homePageSearch.js
@@ -5,6 +5,7 @@ $(document).ready(function () {
 
 
     var earth;
+    var keepAnimating = true;
     function initialize() {
         earth = new WE.map('earth_div');
         earth.setView([20, 0], 2);
@@ -12,23 +13,22 @@ $(document).ready(function () {
         // Start a simple rotation animation
         var before = null;
         requestAnimationFrame(function animate(now) {
+            if(keepAnimating){
             var c = earth.getPosition();
             var elapsed = before ? now - before : 0;
             before = now;
             earth.setCenter([c[0], c[1] + 0.1 * (elapsed / 30)]);
-            requestAnimationFrame(animate);
+           
+                requestAnimationFrame(animate);
+
+            }
         });
     }
 
     initialize();
 
 
-    $.ajax({
-        url: "https://maps.googleapis.com/maps/api/geocode/json?address=Egypt&key=AIzaSyBNoUFS9OOH34ztpgmTmcvb8DPYEBCxxlc", success: function (result) {
-            console.log(result.results[0].geometry.location);
-            dropMarker(result.results[0].geometry.location.lng, result.results[0].geometry.location.lat, "Rohan");
-        }
-    });
+    
 
 
     function dropMarker(long, lat, title, text) {
@@ -60,19 +60,62 @@ $(document).ready(function () {
         }
         ).then(function (data) {
             if (data) {
-                alert("search is done ");
+
+                $.ajax({
+                    url: "https://maps.googleapis.com/maps/api/geocode/json?address="+search_input+"&key=AIzaSyBNoUFS9OOH34ztpgmTmcvb8DPYEBCxxlc", success: function (result) {
+                        // console.log(result.results[0].geometry.location);
+                        if(data.user_location){
+                            keepAnimating = false;
+
+                            dropMarker(result.results[0].geometry.location.lat, result.results[0].geometry.location.lng);
+                            earth.panTo([result.results[0].geometry.location.lat, result.results[0].geometry.location.lng]);
+
+                            // $( "#earth-container" ).animate({
+                            //     opacity: 0.25,
+                            //     left: "+=50",
+                            //     height: "toggle"
+                            //   }, 5000, function() {
+                            //     // Animation complete.
+                            //   });
+                        }
+                        
+                        
+                    }
+                });
+
+                
                 console.log(data)
             }
         });
 
-
-
+        
     }
 
 
 
 
+    $("#earth-container").mouseenter(function(){
+        console.log("mouseenter");
+        keepAnimating = false;
+    })
 
+     $("#earth-container").mouseleave(function(){
+         console.log("mouseleave");
+        keepAnimating = true;
+        var before = null;
+
+        requestAnimationFrame(function animate(now) {
+            if(keepAnimating){
+            var c = earth.getPosition();
+            var elapsed = before ? now - before : 0;
+            before = now;
+            earth.setCenter([c[0], c[1] + 0.1 * (elapsed / 30)]);
+           
+                requestAnimationFrame(animate);
+
+            }
+        });
+    })
 
 
 });
