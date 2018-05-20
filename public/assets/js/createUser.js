899641599706c42f64ba8d89ebd9db0046440ed8
Rohan Menon <rohan@Rohans-MBP.attlocal.net>
2018-05-19 20:29:38 -0500

    rolled back and fixed unmerged code

--- public/assets/js/createUser.js
+++ public/assets/js/createUser.js
@@ -18,7 +18,6 @@ $(document).ready(function () {
         var participant_name = $("#participant_name").val().trim();
         var participant_last_name = $("#participant_last_name").val().trim();
         var participant_email = $("#participant_email").val().trim();
-        var participant_blog = $("#participant_blog").val().trim();
         var participant_pic = $("#participant_pic").val().trim();
         var participant_password = $("#participant_password").val().trim();
 
@@ -32,7 +31,6 @@ $(document).ready(function () {
             user_First_name: participant_name,
             user_Last_name: participant_last_name,
             user_email: participant_email,
-            user_bio_info: participant_blog,
             user_pic_profile: participant_pic,
             user_password: participant_password
         }
@@ -50,7 +48,6 @@ $(document).ready(function () {
         $("#participant_name").val('');
         $("#participant_last_name").val('');
         $("#participant_email").val('');
-        $("#participant_blog").val('');
         $("#participant_pic").val('');
         $("#participant_password").val('');
 
