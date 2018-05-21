a0e88ff5c4c68c1fa0244b90d48e0fe36f179edb
yutaatamura <yutaatamura@gmail.com>
2018-05-20 22:23:17 -0500

    changes

--- public/assets/js/homePageSearch.js
+++ public/assets/js/homePageSearch.js
@@ -62,9 +62,11 @@ $(document).ready(function () {
         }
        
         $.get("/api/homepage/search/"+search_input, function (data, cb) {
+            
             // console.log(data)
         }
         ).then(function (data) {
+            
             if (data) {
 
                 $.ajax({
